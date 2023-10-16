643d73ba5ddf1c926b29b48cf83fc59cfa1fec72
DanieleSpuria <daniele.spuria@proton.me>
2023-10-16 18:35:55 +0200

    add create()

--- src/main/java/org/java/app/api/PizzaRestController.java
+++ src/main/java/org/java/app/api/PizzaRestController.java
@@ -3,6 +3,7 @@ package org.java.app.api;
 import java.util.List;
 import java.util.Optional;
 
+import org.java.app.api.dto.PizzaDto;
 import org.java.app.db.pojo.Pizza;
 import org.java.app.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +36,9 @@ public class PizzaRestController {
 		return new ResponseEntity<List<Pizza>>(pizze, HttpStatus.OK);
 	}
 	
+	
+	
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<Pizza> getPizza(
 				@PathVariable int id
@@ -44,5 +50,26 @@ public class PizzaRestController {
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		
 		return new ResponseEntity<>(optPizza.get(), HttpStatus.OK);
+	}
+	
+	
+	
+	
+	@PostMapping
+	public ResponseEntity<Pizza> create(
+				@RequestBody PizzaDto pizzaDto
+			) {
+		
+		Pizza pizza = new Pizza(
+					pizzaDto.getNome(), 	
+					pizzaDto.getDescrizione(), 
+					pizzaDto.getFoto(), 
+					pizzaDto.getPrezzo()
+				);
+		
+	    pizzaServ.save(pizza);
+		
+		return new ResponseEntity<Pizza>(pizza, HttpStatus.OK);
+		
 	}
 }
