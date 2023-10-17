e8bd9d381d841c0485475886a4396bd4b2abf829
DanieleSpuria <daniele.spuria@proton.me>
2023-10-17 18:15:14 +0200

    add search query

--- src/main/java/org/java/app/api/PizzaRestController.java
+++ src/main/java/org/java/app/api/PizzaRestController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,9 +42,16 @@ public class PizzaRestController {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<Pizza>> getAll() {
+	public ResponseEntity<List<Pizza>> getAll(
+				@RequestParam(required = false, name = "search") String query
+			) {
 		
-		List<Pizza> pizze = pizzaServ.findAll();
+		List<Pizza> pizze = null;
+		
+		if (query == null)
+			pizze = pizzaServ.findAll();
+		else
+			pizze = pizzaServ.findByName(query);
 		
 		return new ResponseEntity<List<Pizza>>(pizze, HttpStatus.OK);
 	}
